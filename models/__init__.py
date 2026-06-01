2e53b70ea9667708f887b190c574c8a7e6097983
Fcsgeo <felipecoriolano.pcrj@gmail.com>
2026-06-01 15:42:49 -0300

    feat: initialize views and models packages with module exports

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,27 @@
-# Models
+﻿from .db import (
+    inserir_linha, atualizar_linha, excluir_linha,
+    consultar_linhas, consultar_historico, obter_linha_por_id,
+    obter_itinerarios, obter_oficios_itinerarios,
+    carregar_servicos, carregar_operadores, carregar_areas_operacionais,
+    carregar_parametros_novos, carregar_caracteristicas, carregar_tipos_sistema,
+    carregar_tipos_veiculo, carregar_grupamentos, carregar_oficios,
+    carregar_assuntos_oficios, carregar_tipologia_rede, carregar_abrangencia_territorial,
+    carregar_geometria_tracado, carregar_hierarquia_atendimento,
+    opcoes, listar_tabela,
+    registrar_log, verify_login, validate_password_strength,
+    generate_reset_token, validate_reset_token, reset_password, get_user_email
+)
+
+__all__ = [
+    "inserir_linha", "atualizar_linha", "excluir_linha",
+    "consultar_linhas", "consultar_historico", "obter_linha_por_id",
+    "obter_itinerarios", "obter_oficios_itinerarios",
+    "carregar_servicos", "carregar_operadores", "carregar_areas_operacionais",
+    "carregar_parametros_novos", "carregar_caracteristicas", "carregar_tipos_sistema",
+    "carregar_tipos_veiculo", "carregar_grupamentos", "carregar_oficios",
+    "carregar_assuntos_oficios", "carregar_tipologia_rede", "carregar_abrangencia_territorial",
+    "carregar_geometria_tracado", "carregar_hierarquia_atendimento",
+    "opcoes", "listar_tabela",
+    "registrar_log", "verify_login", "validate_password_strength",
+    "generate_reset_token", "validate_reset_token", "reset_password", "get_user_email"
+]
--- views/__init__.py
+++ views/__init__.py
@@ -1,1 +1,21 @@
-# Views
+﻿from . import (
+    mod_cadastro,
+    mod_consulta,
+    mod_cadastro_ref,
+    mod_ficha,
+    mod_edicao,
+    mod_historico,
+    mod_usuarios,
+    mod_gtfs,
+)
+
+__all__ = [
+    "mod_cadastro",
+    "mod_consulta",
+    "mod_cadastro_ref",
+    "mod_ficha",
+    "mod_edicao",
+    "mod_historico",
+    "mod_usuarios",
+    "mod_gtfs",
+]
